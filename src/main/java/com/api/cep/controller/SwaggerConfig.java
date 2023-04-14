dd9bf4e075112125e834e2c716210e2073db2807
Vanessa <tauchertvanessa@gmail.com>
2023-04-13 22:23:27 -0300

    Add Swagger

---
+++ src/main/java/com/api/cep/controller/SwaggerConfig.java
@@ -0,0 +1,49 @@
+package com.api.cep.controller;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Configuration
+@OpenAPIDefinition
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customApi() {
+        final String securitySchemeName = "basicAuth";
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API Consulta CEP")
+                        .description("Desafio Bradesco WirePro")
+                        .version("1.0.0")
+                        .contact(new Contact()
+                                .name("Vanessa Oliveira")
+                                .email("tauchertvanessa@gmail.com")
+                                .url("https://www.linkedin.com/in/vanessatauchert/"))
+                )
+                .externalDocs(new ExternalDocumentation()
+                        .url("https://github.com/vanessatauchert/Api-Cep"));
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
