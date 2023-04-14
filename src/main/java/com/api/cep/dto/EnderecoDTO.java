714780027b0cc82f6f8a67c79895f2e97ece4c5b
Vanessa <tauchertvanessa@gmail.com>
2023-04-13 22:25:07 -0300

    Add EnderecoDTO

--- src/main/java/com/api/cep/dto/EnderecoDTO.java
+++ src/main/java/com/api/cep/dto/EnderecoDTO.java
@@ -16,5 +16,32 @@ public class EnderecoDTO {
     private String estado;
     private Double frete;
 
-    public EnderecoDTO(Endereco endereco) {}
+    public EnderecoDTO(Endereco endereco) {
+        cep = endereco.getCep();
+        rua = endereco.getLogradouro();
+        complemento = endereco.getComplemento();
+        bairro = endereco.getBairro();
+        cidade = endereco.getLocalidade();
+        estado = endereco.getUf();
+
+    }
+
+    public Double calcularFrete() {
+        if (estado.equals("SP") || estado.equals("RJ") || estado.equals("MG") || estado.equals("ES")) {
+            return 7.85;
+        } else if (estado.equals("DF") || estado.equals("GO") || estado.equals("MT") || estado.equals("MS")) {
+            return 12.50;
+        } else if (estado.equals("AL") || estado.equals("BA") || estado.equals("CE") || estado.equals("MA") ||
+                estado.equals("PB") || estado.equals("PE") || estado.equals("PI") || estado.equals("RN") ||
+                estado.equals("SE")) {
+            return 15.98;
+        } else if (estado.equals("PR") || estado.equals("SC") || estado.equals("RS")) {
+            return 17.30;
+        } else if (estado.equals("AC") || estado.equals("AP") || estado.equals("AM") || estado.equals("PA") ||
+                estado.equals("RO") || estado.equals("RR") || estado.equals("TO")) {
+            return 20.83;
+        } else {
+            return null;
+        }
+    }
 }
